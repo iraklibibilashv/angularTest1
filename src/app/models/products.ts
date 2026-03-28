80fa7b960a14307aece121879547bbd3afe46513
Default <iraklibilashvili99@gmail.com>
2026-03-28 04:05:05 +0400

    xsadasd1

---
+++ src/app/models/products.ts
@@ -0,0 +1,16 @@
+export class Products{
+    id! : number;
+    name? : string;
+    price? : number;
+    image? : string;
+    vegeterian? : boolean;
+    nuts? : boolean;
+    spiciness? : number;
+    categoryId? : number;
+    
+    
+}
+export class Buttons {
+    id! : number;
+    name? : string;
+}
